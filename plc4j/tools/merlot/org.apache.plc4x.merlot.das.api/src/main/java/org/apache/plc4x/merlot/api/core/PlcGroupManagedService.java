b0717b771fcc4fa517498a09f387fad7ccbc806d
César García <cesar.garcia@ceos.com.ve>
2024-09-11 00:17:34 -0400

    Start of work for group and item loader.

--- plc4j/tools/merlot/org.apache.plc4x.merlot.das.api/src/main/java/org/apache/plc4x/merlot/api/core/PlcGroupManagedService.java
+++ plc4j/tools/merlot/org.apache.plc4x.merlot.das.api/src/main/java/org/apache/plc4x/merlot/api/core/PlcGroupManagedService.java
@@ -16,8 +16,12 @@
  */
 package org.apache.plc4x.merlot.api.core;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.osgi.service.cm.ConfigurationEvent;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ConfigurationListener;
@@ -28,12 +32,34 @@ import org.slf4j.LoggerFactory;
 
 public class PlcGroupManagedService implements ManagedService, ConfigurationListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(PlcGroupManagedService.class);      
-
+    private static final Pattern GROUP_PATTERN =  
+            Pattern.compile("(?<groupDevice>.+),(?<groupDescription>.+),(?<groupScanTime>[0-9]{3,5})");
+    
+    protected static final String GROUP_DEVICE = "groupDevice";
+    protected static final String GROUP_DESCRIPTION = "groupDescription";  
+    protected static final String GROUP_SCANTIME = "groupScanTime";    
+    
+    
     @Override
     public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+        Matcher matcher;
+        String fields;
         if (null != properties){
-            Enumeration<String> keys = properties.keys();            
-            keys.asIterator().forEachRemaining(k -> LOGGER.info("Grupo: " + k));
+            Enumeration<String> keys = properties.keys();   
+            
+            while (keys.hasMoreElements()) {
+                String key = keys.nextElement();
+                if (properties.get(key) instanceof int[]) {
+                } else {
+                    fields = (String) properties.get(key);
+                    if ((matcher = GROUP_PATTERN.matcher(fields)).matches()) {
+                        String groupDevice = matcher.group(GROUP_DEVICE);
+                        String groupDescription = matcher.group(GROUP_DESCRIPTION);
+                        String groupsCANtIME = matcher.group(GROUP_SCANTIME); 
+                        LOGGER.info("{} : {} : {} : {}", key, groupDevice, groupDescription, groupsCANtIME);  
+                    }
+                }
+            }                                  
         }
         
     }
--- plc4j/tools/merlot/org.apache.plc4x.merlot.das.api/src/main/java/org/apache/plc4x/merlot/api/core/PlcItemManagedService.java
+++ plc4j/tools/merlot/org.apache.plc4x.merlot.das.api/src/main/java/org/apache/plc4x/merlot/api/core/PlcItemManagedService.java
@@ -18,6 +18,8 @@ package org.apache.plc4x.merlot.api.core;
 
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.osgi.service.cm.ConfigurationEvent;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ConfigurationListener;
@@ -28,12 +30,35 @@ import org.slf4j.LoggerFactory;
 
 public class PlcItemManagedService implements ManagedService, ConfigurationListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(PlcItemManagedService.class);
+    private static final Pattern ITEM_PATTERN =  
+            Pattern.compile("(?<itemDevice>.+),(?<itemGroup>.+),(?<itemDescription>.+),(?<itemId>.+)"); 
+    
+    protected static final String ITEM_DEVICE = "itemDevice";
+    protected static final String ITEM_GROUP = "itemGroup";     
+    protected static final String ITEM_DESCRIPTION = "itemDescription"; 
+    protected static final String ITEM_ID = "itemId";     
     
     @Override
     public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+        Matcher matcher; 
+        String fields;        
         if (null != properties){
-            Enumeration<String> keys = properties.keys();            
-            keys.asIterator().forEachRemaining(k -> LOGGER.info("Grupo: " + k));
+            Enumeration<String> keys = properties.keys();     
+            
+            while (keys.hasMoreElements()) {
+                String key = keys.nextElement();
+                if (properties.get(key) instanceof int[]) {
+                } else {
+                    fields = (String) properties.get(key);
+                    if ((matcher = ITEM_PATTERN.matcher(fields)).matches()){  
+                        String itemDevice = matcher.group(ITEM_DEVICE);
+                        String itemGroup  = matcher.group(ITEM_GROUP);  
+                        String itemDescription  = matcher.group(ITEM_DESCRIPTION); 
+                        String itemId  = matcher.group(ITEM_ID); 
+                        LOGGER.info("{} : {} : {} : {} : {}", key, itemDevice, itemGroup, itemDescription, itemId );
+                    }                      
+                }                
+            }
         }
     }
 
