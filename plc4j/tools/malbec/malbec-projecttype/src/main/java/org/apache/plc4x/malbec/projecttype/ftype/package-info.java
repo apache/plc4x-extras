df0ac83b82ab32bbde9967ff79f2af81baf84977
César García <cesar.garcia@ceos.com.ve>
2026-01-26 18:11:53 -0400

    Node BOB test.

---
+++ plc4j/tools/malbec/malbec-projecttype/src/main/java/org/apache/plc4x/malbec/projecttype/ftype/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
+ */
+@TemplateRegistration(folder = "Other", content = "PLc4xBobFileTypeTemplate.bob")
+package org.apache.plc4x.malbec.projecttype.ftype;
+
+import org.netbeans.api.templates.TemplateRegistration;
