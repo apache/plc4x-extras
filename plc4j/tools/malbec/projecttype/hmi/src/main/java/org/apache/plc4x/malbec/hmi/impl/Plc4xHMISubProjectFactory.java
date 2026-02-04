1c225bcb316696c126e58190389d496b84ba06ae
César García <cesar.garcia@ceos.com.ve>
2026-02-04 18:39:04 -0400

    New projectype for plc4x.

---
+++ plc4j/tools/malbec/projecttype/hmi/src/main/java/org/apache/plc4x/malbec/hmi/impl/Plc4xHMISubProjectFactory.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.plc4x.malbec.hmi.impl;
+
+import java.io.IOException;
+import org.netbeans.api.project.Project;
+import org.netbeans.spi.project.ProjectFactory;
+import org.netbeans.spi.project.ProjectState;
+import org.openide.filesystems.FileObject;
+import org.openide.util.lookup.ServiceProvider;
+
+@ServiceProvider(service=ProjectFactory.class)
+public class Plc4xHMISubProjectFactory implements ProjectFactory {
+
+    public static final String PROJECT_FILE = "test.bob";    
+    
+    @Override
+    public boolean isProject(FileObject fo) {
+        return fo.getFileObject(PROJECT_FILE) != null;
+    }
+
+    @Override
+    public Project loadProject(FileObject fo, ProjectState ps) throws IOException {
+        return isProject(fo) ? new Plc4xHMISubProject(fo, ps) : null;
+    }
+
+    @Override
+    public void saveProject(Project prjct) throws IOException, ClassCastException {
+        //
+    }
+    
+}
