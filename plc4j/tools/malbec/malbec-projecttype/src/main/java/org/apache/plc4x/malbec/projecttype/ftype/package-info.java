fd2666c2b5998e0d158be07bee19b79fea142a93
César García <cesar.garcia@ceos.com.ve>
2026-02-04 18:39:04 -0400

    New projectype for plc4x.

--- plc4j/tools/malbec/malbec-projecttype/src/main/java/org/apache/plc4x/malbec/projecttype/ftype/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
- */
-@TemplateRegistration(folder = "Other", content = "PLc4xBobFileTypeTemplate.bob")
-package org.apache.plc4x.malbec.projecttype.ftype;
-
-import org.netbeans.api.templates.TemplateRegistration;
